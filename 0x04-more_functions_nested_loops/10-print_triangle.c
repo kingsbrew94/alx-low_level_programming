06d1d4c37a376d0ffd61b23faabd325a3dc74e37
kingsbrew94 <kingsleybrew@gmail.com>
2022-09-15 14:51:49 +0000

    more c functions commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,29 +1,30 @@
 #include "main.h"
 
 /**
-  * print_triangle - prints a triangle of squares per input parameter
-  * @sz: The size of the squares triangle
-  */
-void print_triangle(int sz)
+* print_triangle - prints a triangle of squares per input
+* @size: The size of the squares triangle
+*/
+void print_triangle(int size)
 {
 int i, j, k;
-if (sz <= 0)
+if (size <= 0)
 {
 _putchar('\n');
 }
 else
 {
-for (i = 0; i < sz; i++)
+for (i = 0; i < size; i++)
 {
-for (j = sz - i; j > 1; j--)
+for (j = size - i; j > 1; j--)
 {
-_putchar(32);
+  _putchar(32);
 }
 for (k = 0; k <= i; k++)
 {
-_putchar(35);
+  _putchar(35);
 }
 _putchar('\n');
 }
 }
 }
+
