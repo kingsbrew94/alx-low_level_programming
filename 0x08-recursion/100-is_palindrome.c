bc8b615a17b677afd9287badf3d40ca9c7db07a3
kingsbrew94 <kingsleybrew@gmail.com>
2022-09-28 11:46:48 +0000

    Recursive C Commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,54 +1,54 @@
 #include "main.h"
 
-int check_palindrome(char *s);
+int _strlen(char *s);
+int check_palindrome(char *s, int len, int i);
+int is_palindrome(char *s);
 
 /**
-  * is_palindrome - Returns 1 if a string is a palindrome and 0 if not
-  * @s: Holds the string value to be checked
-  *
-  * Return: 1 if a string is a palindrome else 0
-  */
+ * _strlen - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+int length = 0;
+if (*(s + length))
+{
+length++;
+length += _strlen(s + length);
+}
+return (length);
+}
+
+/**
+ * check_palindrome - Checks if a string is palindrome or not
+ * @s: Holds the string value to be checked
+ * @len: Holds the length value of @s.
+ * @i: Holds the index of the string to be checked.
+ *
+ * Return: 1 if a string is a palindrome else 0
+ */
+int check_palindrome(char *s, int len, int i)
+{
+if (s[i] == s[len / 2])
+return (1);
+if (s[i] == s[len - i - 1])
+return (check_palindrome(s, len, i + 1));
+return (0);
+}
+
+/**
+ * is_palindrome - Returns 1 if a string is a palindrome and 0 if not
+ * @s: Holds the string value to be checked
+ *
+ * Return: 1 if a string is a palindrome else 0
+ */
 int is_palindrome(char *s)
 {
-if (*s == '0')
+int i = 0;
+int strlen = _strlen(s);
+if (!(*s))
 return (1);
-return (check_palindrome(s));
+return (check_palindrome(s, strlen, i));
 }
-
-/**
-  * check_palindrome - Checks if a string is palindrome or not
-  * @s: Holds the string value to be checked
-  *
-  * Return: 1 if a string is a palindrome else 0
-  */
-int check_palindrome(char *s)
-{
-int l = _strlen_recursion(s) - 1;
-if (*s == s[l])
-{
-s++;
-l--;
-}
-else
-{
-return (0);
-}
-return (1);
-}
-
-/**
-  * _strlen_recursion - Gets the length of a string
-  * @s: Holds the string to get the length
-  *
-  * Return: the string length
-  */
-int _strlen_recursion(char *s)
-{
-if (*s == '\0')
-{
-return (0);
-}
-s++;
-return (_strlen_recursion(s) + 1);
-}
-
