4817b5d220456274cab117aa5c8ec05712b30a38
kingsbrew94 <kingsleybrew@gmail.com>
2022-09-30 21:47:00 +0000

    argv, argc command commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * main - prints the minimum number of coins to make change for an amount of money.
+ * main - prints the minimum number of coins to make change
+ * for an amount of money.
  * @argc: arguments size.
  * @argv: arguments.
  *
